72604325313295cc463fcb59179a708409041fa7
Anjali Anand Heda <139965050+anjaliheda@users.noreply.github.com>
2025-04-04 11:55:55 +0530

    Add files via upload

--- LeakyBucket.c
+++ LeakyBucket.c
@@ -10,34 +10,29 @@ void leakyBucket(int *bucket, int packetSize, int bucketSize, int outputRate) {
     if (*bucket + packetSize > bucketSize) {
         printf("\nBucket Overflow! Packet of size %d bytes is dropped.\n", packetSize);
     } else {
-        *bucket += packetSize;  // Add packet to the bucket
+        *bucket += packetSize;  
         printf("\nPacket of size %d bytes added to the bucket.\n", packetSize);
     }
 
     // Output data at the constant rate (leaky rate)
     while (*bucket > 0) {
-        sleep(1);  // Simulate 1-second delay for output
+        sleep(1);  /
         time++;
 
         if (*bucket >= outputRate) {
-            *bucket -= outputRate;  // Output the fixed rate of data
+            *bucket -= outputRate;  
             printf("\nTime %d Sec: %d bytes Outputted from bucket.\n", time, outputRate);
         } else {
             printf("\nTime %d Sec: %d bytes Outputted (last packet).\n", time, *bucket);
-            *bucket = 0;  // Empty the bucket after this output
+            *bucket = 0;  
         }
     }
     printf("\nBucket Output Successful.\n");
 }
 
 int main() {
-    int bucket = 0;  // Initial bucket size is 0
+    int bucket = 0;  
     int bucketSize, outputRate, numPackets, packetSize, delay, time = 0;
-
-    // User input for bucket size, output rate, and number of packets
-    printf("____________________________________________\n");
-    printf("C Program for Leaky Bucket Algorithm\n");
-    printf("____________________________________________\n");
 
     printf("Enter Bucket Size (Maximum Capacity): ");
     scanf("%d", &bucketSize);
@@ -54,13 +49,12 @@ int main() {
         sleep(delay);
         time += delay;
 
-        packetSize = rand() % 1000;  // Random packet size (0-999 bytes)
+        packetSize = rand() % 1000;  
         printf("\n---------------------------\n");
         printf("Time %d Sec\n", time);
         printf("Packet Number %d\n", i);
         printf("Packet Size %d bytes\n", packetSize);
 
-        // Call the leakyBucket function to process the packet
         leakyBucket(&bucket, packetSize, bucketSize, outputRate);
     }
 
