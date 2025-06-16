64eb14c844ce2556b811a49278761bacbdfd91e1
Anjali Anand Heda <139965050+anjaliheda@users.noreply.github.com>
2025-06-16 23:31:19 +0530

    Update Caesar_Cipher.c

--- Caesar_Cipher.c
+++ Caesar_Cipher.c
@@ -1,70 +1,49 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 
 int main() {
-
     char text[500], ch;
     int key, choice;
-    printf("1. Encrypt \n2. Decrypt \nEnter your choice: ");
+
+    printf("1. Encrypt\n2. Decrypt\nEnter your choice: ");
     scanf("%d", &choice);
-    printf("Enter a message: ");
-    scanf("%s", text);
+    printf("Enter a message (no special characters): ");
+    scanf(" %s", text);  
     printf("Enter the key: ");
     scanf("%d", &key);
 
-    for (int i = 0; text[i] != '\0'; ++i) {
+    for (int i = 0; text[i] != '\0'; i++) {
         ch = text[i];
 
-        if (isalnum(ch)) {
-
-            if (choice == 1) {  // Encryption
-              
-                if (islower(ch)) {
-                    ch = (ch - 'a' + key) % 26 + 'a';
-                }
-                
-                else if (isupper(ch)) {
-                    ch = (ch - 'A' + key) % 26 + 'A';
-                }
-                
-                else if (isdigit(ch)) {
-                    ch = (ch - '0' + key) % 10 + '0';
-                }
-            }
-            else if (choice == 2) {  // Decryption
-                
-                if (islower(ch)) {
-                    ch = (ch - 'a' - key + 26) % 26 + 'a';
-                }
-               
-                else if (isupper(ch)) {
-                    ch = (ch - 'A' - key + 26) % 26 + 'A';
-                }
-                
-                else if (isdigit(ch)) {
-                    ch = (ch - '0' - key + 10) % 10 + '0';
-                }
-            } else {
-                printf("Invalid choice! Please enter 1 or 2.");
-                return 1;
-            }
-
-        } else {
-            printf("Invalid character in the message.\n");
+        if (islower(ch)) {
+            ch = (choice == 1)
+                ? (ch - 'a' + key) % 26 + 'a'
+                : (ch - 'a' - key + 26) % 26 + 'a';
+        }
+        else if (isupper(ch)) {
+            ch = (choice == 1)
+                ? (ch - 'A' + key) % 26 + 'A'
+                : (ch - 'A' - key + 26) % 26 + 'A';
+        }
+        else if (isdigit(ch)) {
+            ch = (choice == 1)
+                ? (ch - '0' + key) % 10 + '0'
+                : (ch - '0' - key + 10) % 10 + '0';
+        }
+        else if (ch != ' ') {
+            printf("Error: Special characters not allowed.\n");
             return 1;
         }
 
-        
-        text[i] = ch;
+        text[i] = ch;  
     }
 
-   
-    if (choice == 1) {
+    if (choice == 1)
         printf("Encrypted message: %s\n", text);
-    } else if (choice == 2) {
+    else if (choice == 2)
         printf("Decrypted message: %s\n", text);
-    }
+    else
+        printf("Invalid choice.\n");
 
     return 0;
 }
-
